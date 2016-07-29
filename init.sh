fb0b27282505c5ca3b912fef53234fa3335c0b08
Xiaohan Zhang <xiaohan@commonsku.com>
2016-07-29 16:28:18 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-get="sudo apt-get install -y"
-$get vim git bzr subversion default-jdk automake curl
+get="apt-get install -y"
+$get vim-gnome git automake curl wget htop cmake
+$get silversearcher-ag
 $get python python-dev python-setuptools python-pip python-virtualenv
-$get libreadline-dev
 
-git config --global core.editor "vim"
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+# git config --global core.editor "vim"
+# git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 
-mv ~/.vim vim.old
-mv ~/.vimrc vimrc.old
-ln -sf ~/init/ackrc ~/.ackrc
-ln -sf ~/init/vim ~/.vim
-ln -sf ~/.vim/.vimrc ~/.vimrc
+# ln -sf ~/init/vim ~/.vim
+# ln -sf ~/.vim/.vimrc ~/.vimrc
